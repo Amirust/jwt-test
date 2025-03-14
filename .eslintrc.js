03c3d5c831e398b8fcd90b751027acba69e2cc9c
Mira <r1n0ku@yandex.ru>
2025-03-14 15:33:05 +0100

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,6 @@ module.exports = {
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
     'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
   ],
   root: true,
   env: {
@@ -21,5 +20,10 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'array-bracket-spacing':  ["error", "always", { "objectsInArrays": false }],
+    'quotes': ["error", "single"],
+    'no-trailing-spaces': "error",
+    'object-curly-spacing': ["error", "always"],
+    'indent': ["error", 2],
   },
 };
